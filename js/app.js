9946cfeb2fac091c451a0d03129fd80da16840c3
valeriavalles <valeria.sv@outlook.es>
2017-11-29 02:56:23 -0500

    añadiendo evento al overview/students

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 /*
  * Funcionalidad de tu producto
  */
+window.addEventListener('load', function() {
+  var student = document.getElementById('student');// al quiero hacer click
+  var container1 = document.getElementById('container1');// al que quiero que aparezca
+  var overview = document.getElementById('overview');
+  var overcon = document.getElementById('overcon');
+
+  student.addEventListener('click', elige);
+  function elige() {
+    container1.style.display = 'block';
+    overview.style.display = 'none';
+  }
+  overcon.addEventListener('click', over);
+  function over() {
+    overview.style.display = 'block';
+  }
+});
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
